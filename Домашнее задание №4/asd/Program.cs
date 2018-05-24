5deaf9edb45a8a7063783b444a17b2488ad270ec
Kyrzin <kent777da@ukr.net>
2018-05-24 19:41:46 +0300

    zxc

---
+++ Домашнее задание №4/asd/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start:
+            Console.WriteLine("Введи любое число от 0 до 100");
+            int asd = Convert.ToInt32(Console.ReadLine());
+            if (asd >= 0 && asd <= 14)
+            {
+                Console.WriteLine("Число между 0 и 14");
+            }
+            else if (asd >= 15 && asd <= 35)     
+            {
+                Console.WriteLine("Число между 15 и 35");      
+            }
+            else if (asd >= 36 && asd <= 50)
+            {
+                Console.WriteLine("Число между 36 и 50");
+            }
+            else if (asd >= 50 && asd <= 100)
+            {
+                Console.WriteLine("Число между 50 и 100");
+            }
+            else
+            {
+                Console.WriteLine("Хз какое число,Сказали же от 0 до 100!!!");
+            }
+            Console.WriteLine("Жилаете продолжить??");
+            string dsa = Console.ReadLine();
+            if (dsa == "y")
+            {
+                goto Start;
+            }
+            else { return; }
+
+        
+        
+        }
+    }
+}
---
+++ Домашнее задание №4/Домашнее задание №4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Домашнее_задание__4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start:
+            double operand1, operand2,asd=2;
+            Console.WriteLine("Введите первое значение!");
+            operand1 = Convert.ToDouble(Console.ReadLine());
+            string sign;
+            Console.WriteLine("Что будем с ним делать?");
+            sign =Console.ReadLine();
+            Console.WriteLine("Введите второе значение!");
+            operand2 = Convert.ToDouble(Console.ReadLine());
+            switch (sign)
+            {
+                case "+": asd = operand1 + operand2;
+                    break;
+                case "-": asd = operand1 - operand2;
+                    break;
+                case "*": asd = operand1 * operand2;
+                    break;
+                case "/": asd=operand1 / operand2;
+                    if (operand2==0)
+                    {
+                        Console.WriteLine("На ноль делить нельзя!!!Нажмите 'У' если хотите начать заново ");
+                        string dsa = Console.ReadLine();
+                        if (dsa == "y")
+                        {
+                            goto Start;
+                        }
+                        else { return; }
+                    }
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine("Операция {0} \nРезультат {1}",sign,asd);
+            Console.ReadKey();
+        }
+    }
+}
